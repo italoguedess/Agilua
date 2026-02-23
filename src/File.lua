c4bd98d7577ba189a9913c5a21df66547979ce78
Italo Guedes <italoguedes@alu.ufc.br>
2026-02-23 21:51:19 +0100

    [FIX] Yet another string match fix

--- src/File.lua
+++ src/File.lua
@@ -17,7 +17,7 @@ function File.read_cards(fname)
 	local cards = {}
 	for i = 1, #card_strings, 1 do
 		local card = {}
-		card.title = string.sub(string.match(card_strings[i], "%u%p%s.+[%a:][:]%s"), 4, -5)
+		card.title = string.sub(string.match(card_strings[i], "%u%p%s.+%s[:][%w:]"), 4, -4)
 		card.status = string.match(card_strings[i], "[%a_]+")
 		card.priority = string.sub(string.match(card_strings[i], "%p%u"), 2)
 		-- {or " "} in case string match returns nil meaning there are no tags
